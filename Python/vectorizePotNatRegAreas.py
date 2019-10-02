f9e16d28df0cf8586ac21da211d2b84b8f4bf7a8
Felipe Barros <felipe.b4rros@gmail.com>
2019-10-02 15:25:17 -0300

    vectorization process done!

--- Python/vectorizePotNatRegAreas.py
+++ Python/vectorizePotNatRegAreas.py
@@ -10,47 +10,6 @@ ee.Initialize()
 potAreas = ee.Image("users/HotSpotRestoration/PotentialNaturalRegeneration/PotRestArea00")
 world = ee.FeatureCollection("USDOS/LSIB_SIMPLE/2017")
 
-NatGrass1992
-PotRestArea00
-PredRF_Finv9_mergeArea_v9pass3_2
-PredRF_Finv9_mergeArea_v9pass3_3
-PredRF_Finv9_mergeArea_v9pass3_4
-PredRF_Finv9_mergeArea_v9pass3_5
-PredRF_Finv9_mergeArea_v9pass3_6
-PredRF_Finv9_mergeArea_v9pass3_7
-PredRF_Finv9_mergeArea_v9pass3_8
-RegenerationRaster
-random_regen
-CropPastures
-ESACCI-LC-L4-LCCS-Map-300m-P1Y-1992_2015-v207
-ESACCI-LC-L4-LCCS-Map-300m-P1Y-2015-v207
-ESAUrbanArea
-ESAUrbanArea2
-GlobalForestClasification
-GovernanceF32_pxl
-GovernancePolygon
-HumanFootPrint
-IDH
-RuralPopDistribution
-RuralPovertyDensity
-Soil_CEC_1km
-VALIDATION_stats2ndRound
-grip4_total_dens_m_km2
-grip4_tp1_dens_m_km2
-grip4_tp2_dens_m_km2
-grip4_tp3_dens_m_km2
-grip4_tp4_dens_m_km2
-grip4_tp5_dens_m_km2
-landuse2015_300m_WGS84
-opportunity_costs_comb_900m_Molweide_WGS84
-soil_BLDFIE_30cm_1km
-soil_PHIHOX_30cm_1km
-soil_PHIKCL_30cm_1km
-soil_SNDPPT_30cm_1km
-New Script *
-
-Use print(...) to write to this console.
-
 table = ee.FeatureCollection("users/HotSpotRestoration/PotentialNaturalRegeneration/PredRF_Finv9_mergeArea_v9pass3_2")
 table2 = ee.FeatureCollection("users/HotSpotRestoration/PotentialNaturalRegeneration/PredRF_Finv9_mergeArea_v9pass3_3")
 table3 = ee.FeatureCollection("users/HotSpotRestoration/PotentialNaturalRegeneration/PredRF_Finv9_mergeArea_v9pass3_4")
@@ -59,24 +18,80 @@ table5 = ee.FeatureCollection("users/HotSpotRestoration/PotentialNaturalRegenera
 table6 = ee.FeatureCollection("users/HotSpotRestoration/PotentialNaturalRegeneration/PredRF_Finv9_mergeArea_v9pass3_7")
 table7 = ee.FeatureCollection("users/HotSpotRestoration/PotentialNaturalRegeneration/PredRF_Finv9_mergeArea_v9pass3_8")
 
+# change the geometry to each table
+vecpotareas3_2 = potAreas.reduceToVectors(geometry = table.geometry().bounds(),
+                                          scale =  300,
+                                          bestEffort = True,
+                                          tileScale = 8)
+vecpotareas3_3 = potAreas.reduceToVectors(geometry = table2.geometry().bounds(),
+                                          scale =  300,
+                                          bestEffort = True,
+                                          tileScale = 8)
+vecpotareas3_4 = potAreas.reduceToVectors(geometry = table3.geometry().bounds(),
+                                          scale =  300,
+                                          bestEffort = True,
+                                          tileScale = 8)
+vecpotareas3_5 = potAreas.reduceToVectors(geometry = table4.geometry().bounds(),
+                                          scale =  300,
+                                          bestEffort = True,
+                                          tileScale = 8)
+vecpotareas3_6 = potAreas.reduceToVectors(geometry = table5.geometry().bounds(),
+                                          scale =  300,
+                                          bestEffort = True,
+                                          tileScale = 8)
+vecpotareas3_7 = potAreas.reduceToVectors(geometry = table6.geometry().bounds(),
+                                          scale =  300,
+                                          bestEffort = True,
+                                          tileScale = 8)
+vecpotareas3_8 = potAreas.reduceToVectors(geometry = table7.geometry().bounds(),
+                                          scale =  300,
+                                          bestEffort = True,
+                                          tileScale = 8)
 
-vecpotareas = potAreas.reduceToVectors(#reducer = ee.Reducer.first(),
-                                                 geometry = table.geometry().bounds(),
-                                                                     scale =  300,
-                                                                              #'geometryType', eightConnected, labelProperty, crs, crsTransform,
-                                                                              bestEffort = True,
-#maxPixels,
-tileScale = 8 #, geometryInNativeProjection
-)
 
-# process tasks
-outvecpotareas = batch.Export.table.toAsset(\
-    collection = vecpotareas,
-    description = "teste description",
-    assetId = "users/HotSpotRestoration/PotentialNaturalRegeneration/vecpotareas00")
+# process Output tasks
+outvecpotareas2 = batch.Export.table.toAsset(\
+    collection = vecpotareas3_2,
+    description = "",
+    assetId = "users/HotSpotRestoration/PotentialNaturalRegeneration/vecpotareas00_3_2")
+outvecpotareas3 = batch.Export.table.toAsset(\
+    collection = vecpotareas3_3,
+    description = "",
+    assetId = "users/HotSpotRestoration/PotentialNaturalRegeneration/vecpotareas00_3_3")
+outvecpotareas4 = batch.Export.table.toAsset(\
+    collection = vecpotareas3_4,
+    description = "",
+    assetId = "users/HotSpotRestoration/PotentialNaturalRegeneration/vecpotareas00_3_4")
+outvecpotareas5 = batch.Export.table.toAsset(\
+    collection = vecpotareas3_5,
+    description = "",
+    assetId = "users/HotSpotRestoration/PotentialNaturalRegeneration/vecpotareas00_3_5")
+outvecpotareas6 = batch.Export.table.toAsset(\
+    collection = vecpotareas3_6,
+    description = "",
+    assetId = "users/HotSpotRestoration/PotentialNaturalRegeneration/vecpotareas00_3_6")
+outvecpotareas7 = batch.Export.table.toAsset(\
+    collection = vecpotareas3_7,
+    description = "",
+    assetId = "users/HotSpotRestoration/PotentialNaturalRegeneration/vecpotareas00_3_7")
+outvecpotareas8 = batch.Export.table.toAsset(\
+    collection = vecpotareas3_8,
+    description = "",
+    assetId = "users/HotSpotRestoration/PotentialNaturalRegeneration/vecpotareas00_3_8")
 
-process = batch.Task.start(outvecpotareas)
+process3_2 = batch.Task.start(outvecpotareas2)
+process3_3 = batch.Task.start(outvecpotareas3)
+process3_4 = batch.Task.start(outvecpotareas4)
+process3_5 = batch.Task.start(outvecpotareas5)
+process3_6 = batch.Task.start(outvecpotareas6)
+process3_7 = batch.Task.start(outvecpotareas7)
 
 # checking task status
-outvecpotareas.status()
-pprint(batch.Task.status(outvecpotareas))
+#outvecpotareas.status()
+#pprint(batch.Task.status(outvecpotareas))
+outvecpotareas2.status()
+outvecpotareas3.status()
+outvecpotareas4.status()
+outvecpotareas5.status()
+outvecpotareas6.status()
+outvecpotareas7.status()
